9b9a213d9ce927a1ac1f3d80b51bbac4e106ecbd
toonami2907 <ikemm50@gmail.com>
2024-04-13 08:42:27 -0700

    new pushing

--- api/index.js
+++ api/index.js
@@ -3,14 +3,14 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import path from 'path'
-import {fileURLToPath} from 'url'
+// import {fileURLToPath} from 'url'
 import userRoute from './routes/User.route.js'
 import cookieParser from 'cookie-parser';
 import productRoute from './routes/Product.route.js'
 const app = express();
 config();
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.resolve();
 console.log(__dirname)
 const PORT = 3000
 // middleware
@@ -19,8 +19,8 @@ app.use(express.static(path.join(__dirname, '/client/dist')))
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("*", (_, res) => {
-  res.sendFile(path.join(__dirname, '/client/dist/index.html'))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
 });
 // routes
 app.use('/api/Product', productRoute);
