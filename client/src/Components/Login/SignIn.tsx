49349fdbc9b90be058534259ee7846c8239ef189
Effiong Michael Ikem <115380518+toonami2907@users.noreply.github.com>
2024-04-14 06:36:37 -0700

    Update SignIn.tsx

--- client/src/Components/Login/SignIn.tsx
+++ client/src/Components/Login/SignIn.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom"
+import {  useNavigate } from "react-router-dom"
 import { signInStart, signInFailure, signInSuccess } from "../../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { OAuth } from "../OAuth";
@@ -39,24 +39,35 @@ export const SignIn = () => {
     }
 
     return (
-        <div className="h-[400px] ">
-            <div className="w-[80%] mx-auto h-full flex  flex-col justify-center items-center" >
-                <h1 className="text-2xl font-semibold  text-black font-serif mb-1">Login Here</h1>
-                <form onSubmit={handleSubmit} className="w-[60%] h-[50%] gap-3 flex flex-col justify-center items-center mx-auto">
-                    <input type="email" id="email" onChange={handleChange} placeholder="Email" className="bg-slate-100 rounded-lg w-[300px] lg:w-[70%] p-3 " />
-                    <input type="Password" onChange={handleChange} id="password" placeholder="Password" className="bg-slate-100 rounded-lg w-[300px] lg:w-[70%] p-3 " />
-                    <button className="p-3 bg-slate-900 w-[70%] disabled:bg-slate-700 rounded-lg text-white hover:bg-orange-600">  {loading ? 'Loading....' : 'SIGN IN'}</button>
-                    <OAuth />
-                    <Link className="text-lg mb-4" to='/SignUp'>Create an Account</Link>
-                </form>
-                <p className="text-red-700 mt-5 text-lg">
-                    {error && 'Something went wrong!'}
-                </p>
-
-
-
-
+        <div className="flex flex-col items-center pt-6">
+        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700" >
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">Login into your account</h1>
+        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+        <div>
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+              <input type="email" onChange={handleChange} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Emelia_Erickson@.gmail.com" required />
             </div>
+        
+            <div>
+              <label htmlFor="password"  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" >Password</label>
+              <input onChange={handleChange}  type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+            </div>
+            <button type="submit"  disabled={loading} className="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">{loading ? 'loading....' : 'Login into an account'}</button>
+            <div className="flex flex-col justify-center items-center"> 
+            <OAuth/>
+            </div>
+            <p className="text-sm font-light text-gray-500 dark:text-gray-400">Already have an account? <a className="font-medium text-blue-600 hover:underline dark:text-blue-500" href="/signup">Sign up here</a></p>
+        </form>
+          {!error && (
+            <p className="text-red-700 mt-5 ">
+              Something went wrong. Please try again.
+            </p>
+          )}
+  
         </div>
+        </div>
+      </div>
     )
 }
+
